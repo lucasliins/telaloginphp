083093cbc55b14b5349010fbbee9c12b533e343b
lucasliins <lucaslins_12@hotmail.com>
2023-04-03 23:31:35 -0300

    Segunda parte

--- conexao.php
+++ conexao.php
@@ -1,12 +1,13 @@
 <?php
 
-$usuario = root; 
+$usuario = 'root'; 
 $senha = ''; 
 $database = 'login'; 
 $host = 'localhost'; 
 
-mysqli = new mysqli($host, $usuario, $senha, $database); 
+$mysqli = new mysqli($host, $usuario, $senha, $database); 
 
 if($mysqli->error){
     die("Falha ao conectar ao banco de dados" . mysqli->error); 
 }
+?>
--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php 
 include('conexao.php');
+if(isset($_POST['email']) || isset($_POST['senha'])){
+    if(strlen($_POST['email']) == 0){
+        echo "Preencha seu email:";
+    } elseif(strlen($_POST['senha']) == 0){
+      echo "Preencha sua senha";   
+    } else {
+        $email = mysqli->real_escape_string($_POST['email']);
+        $email = mysqli->real_escape_string($_POST['senha']);
+
+        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+        $sql_query = mysqli->query($sql_code) or die("Falha na execução" . $mysqli->error);
+
+        $quantidade = $sql_query->num_rows;
+
+        if($quantidade == 1){
+            $usuario = $sql_query->fetch_assoc(); 
+        }
+    }
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
